f2724973f7ecedec4a638a56e4dbd4a22cea1a4f
Mona Essaid <123646726+monaessaid@users.noreply.github.com>
2025-12-30 00:53:39 -0800

    Time: 3 ms (59.36%), Space: 47 MB (49.95%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,12 +1,26 @@
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         for(int i= 0; i< nums.length; i++) {
+//             for (int j = i + 1; j< nums.length; j++){
+//                 if (nums[i] + nums[j] == target) {
+//                     return new int[]{i,j};               
+//                 }
+//             }
+//         }
+//         return nums;
+//     }
+// }
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer>  pairs = new HashMap<>();
         for(int i= 0; i< nums.length; i++) {
-            for (int j = i + 1; j< nums.length; j++){
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i,j};               
-                }
-            }
-        }
-        return nums;
+            Integer numberIndex = pairs.get(nums[i]);
+                if (numberIndex != null) {
+                    return new int[]{i, numberIndex};
+                } 
+                pairs.put(target- nums[i], i);
     }
+    return nums;
+}
 }
